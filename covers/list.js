e707ec975a60b5d33d638f4f10909260daf67da7
thaDoom <58373889+thadoom@users.noreply.github.com>
2026-07-07 03:23:53 +0200

    Update list.js

--- covers/list.js
+++ covers/list.js
@@ -1,8 +1,8 @@
 window.NJ_GAMES=[
   {name:'League of Legends',cover:'league-of-legends.png'},
   {name:'Mafia: Old Town',cover:'mafia-old-town.jpg'},
-  {name:'Valorant',cover:'valorant.jpg'},
-  {name:'Just Chatting',cover:''},
+  {name:'Valorant',cover:'vvalorant.png'},
+  {name:'Just Chatting',cover:'chatting.jpg'},
   {name:'Stardew Valley',cover:'stardew-valley.jpg'},
   {name:'Ori & The Blind Forest',cover:'ori-and-the-blind-forest.jpg'}
 ];
