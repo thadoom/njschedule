16be6881368a2d78ab73bc34acad32595163e6e6
thaDoom <58373889+thadoom@users.noreply.github.com>
2026-07-07 03:17:33 +0200

    Map activities to game covers

--- covers/list.js
+++ covers/list.js
@@ -1,7 +1,8 @@
-window.NJ_COVERS=[
-  'league-of-legends.png',
-  'mafia-old-town.jpg',
-  'valorant.jpg',
-  'stardew-valley.jpg',
-  'ori-and-the-blind-forest.jpg'
+window.NJ_GAMES=[
+  {name:'League of Legends',cover:'league-of-legends.png'},
+  {name:'Mafia: Old Town',cover:'mafia-old-town.jpg'},
+  {name:'Valorant',cover:'valorant.jpg'},
+  {name:'Just Chatting',cover:''},
+  {name:'Stardew Valley',cover:'stardew-valley.jpg'},
+  {name:'Ori & The Blind Forest',cover:'ori-and-the-blind-forest.jpg'}
 ];
